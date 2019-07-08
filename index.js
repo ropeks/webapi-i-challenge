3d556bb8881d931d594b6c8c2845beef9bac00a0
ropeks <ropeksmilioner@gmail.com>
2019-07-08 15:04:29 +0200

    create GET request endpoint

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+const port = 3000;
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(data => {
+            res.status(200)
+                .json(data);
+        })
+        .catch(err => {
+            res.status(500)
+                .json({ err: 'Information cannot be retrieved' });
+        })
+});
+
+server.listen(port, () => {
+    console.log('Listening on port ' + port);
+})
