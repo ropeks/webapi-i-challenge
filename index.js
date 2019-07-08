841274ffcb0f26942a7071e22dd423d83833a2b8
ropeks <ropeksmilioner@gmail.com>
2019-07-08 16:18:54 +0200

    create post request endpoint

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.get('/api/users', (req, res) => {
         })
         .catch(err => {
             res.status(500)
-                .json({ err: 'Information cannot be retrieved' });
+                .json({ error: 'Information cannot be retrieved' });
         })
 });
 
@@ -26,12 +26,12 @@ server.get('/api/users/:id', (req, res) => {
                     .json(data);
             } else {
                 res.status(404)
-                    .json({ message: 'The user with the specified ID does not exist.' })
+                    .json({ error: 'The user with the specified ID does not exist.' })
             }
         })
         .catch(err => {
             res.status(500)
-                .json({ err: 'Information cannot be retrieved' });
+                .json({ error: 'Information cannot be retrieved' });
         })
 });
 
@@ -43,14 +43,32 @@ server.delete('/api/users/:id', (req, res) => {
                     .json(data)
             } else {
                 res.status(404)
-                    .json({ message: 'The user with the specified ID does not exist.' })
+                    .json({ error: 'The user with the specified ID does not exist.' })
             }
         })
         .catch(err => {
             res.status(500)
-                .json({ err: 'Information cannot be retrieved' })
+                .json({ error: 'Information cannot be retrieved' })
         });
 });
+
+server.post('/api/users/', (req, res) => {
+    const { name, bio } = req.body;
+    if ( name || bio ) {
+        db.insert(req.body)
+            .then(data => {
+                res.status(201)
+                    .json(data)
+            })
+            .catch(err => {
+                res.status(500)
+                    .json({ error: 'There was an error while saving the user to the database' })
+            })
+    } else {
+        res.status(400)
+            .json({ error: 'Please provide name and bio for the user.' })
+    };
+})
 
 server.listen(port, () => {
     console.log('Listening on port ' + port);
